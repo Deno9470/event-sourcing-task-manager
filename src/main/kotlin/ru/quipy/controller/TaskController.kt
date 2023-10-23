b8d25edff81b285607a830bfcbdde42b09bfe9ed
Deno9470 <Deno9470@mail.ru>
2023-10-23 03:20:01 +0300

    feat: task module

---
+++ src/main/kotlin/ru/quipy/controller/TaskController.kt
@@ -0,0 +1,71 @@
+package ru.quipy.controller
+
+import org.springframework.web.bind.annotation.*
+import ru.quipy.api.aggregates.TaskAggregate
+import ru.quipy.api.aggregatesEvents.*
+import ru.quipy.core.EventSourcingService
+import ru.quipy.logic.aggregateCommands.*
+import ru.quipy.logic.aggregateStates.TaskAggregateState
+import java.util.*
+
+//Создайте REST API для
+// создания,
+// измнения
+// получения последней версии агрегатов по его ID.
+
+
+@RestController
+@RequestMapping("/tasks")
+class TaskController(
+    val taskEsService: EventSourcingService<UUID, TaskAggregate, TaskAggregateState>
+) {
+    @GetMapping("")
+    fun getAllTasks() : List<TaskAggregateState> {
+        throw IllegalAccessException()
+    }
+
+    @GetMapping("/{taskId}")
+    fun getTask(@PathVariable taskId: UUID) : TaskAggregateState? {
+        return taskEsService.getState(taskId)
+    }
+
+    @PostMapping("")
+    fun createTask(@RequestBody taskName: String, @RequestBody description: String, @RequestBody parentProjectId : UUID) : TaskCreatedEvent {
+        return taskEsService.create {
+            it.createTask(
+                taskName = taskName,
+                description = description,
+                statusId = UUID.fromString(""),    // TODO Implement static StatusId Created
+                parentProjectId = parentProjectId
+            )
+        }
+    }
+    @PatchMapping("/{taskId}")
+    fun renameTask(@PathVariable taskId: UUID, @RequestBody newTaskName : String) : TaskRenamedEvent {
+        return taskEsService.update(taskId) {
+            it.renameTask(newTaskName)
+        }
+    }
+
+    @PostMapping("/{taskId}/executors/{userId}")
+    fun addExecutor(@PathVariable taskId: UUID, @PathVariable userId : UUID) : ExecutorAddedEvent {
+        return taskEsService.update(taskId) {
+            it.addExecutor(userId)
+        }
+    }
+
+    @DeleteMapping("/{taskId}/executors/{userId}")
+    fun removeExecutor(@PathVariable taskId: UUID,  @PathVariable userId : UUID) : ExecutorRemovedEvent {
+        return taskEsService.update(taskId) {
+            it.removeExecutor(userId)
+        }
+    }
+    @PatchMapping("/{taskId}/status")
+    fun changeStatus(@PathVariable taskId : UUID, @RequestBody statusId : UUID) : TaskStatusChangedEvent {
+        return taskEsService.update(taskId) {
+            it.changeStatus(statusId)
+        }
+    }
+
+
+}
