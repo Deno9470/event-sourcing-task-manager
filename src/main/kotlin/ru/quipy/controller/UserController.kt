a3c7cdbad215964d58ee9d7000d6444f7758b36a
Deno9470 <Deno9470@mail.ru>
2023-10-23 03:19:46 +0300

    feat: user commands

---
+++ src/main/kotlin/ru/quipy/controller/UserController.kt
@@ -0,0 +1,34 @@
+package ru.quipy.controller
+
+import org.springframework.web.bind.annotation.*
+import ru.quipy.api.aggregates.UserAggregate
+import ru.quipy.api.aggregatesEvents.UserRegisteredEvent
+import ru.quipy.core.EventSourcingService
+import ru.quipy.logic.aggregateCommands.registerUser
+import ru.quipy.logic.aggregateStates.UserAggregateState
+import java.util.*
+
+//Создайте REST API для
+// создания,
+// измнения
+// получения последней версии агрегатов по его ID.
+
+
+@RestController
+@RequestMapping("/users")
+class UserController(
+    val userEsService: EventSourcingService<UUID, UserAggregate, UserAggregateState>
+) {
+
+    @GetMapping("/")
+    fun getAllUsers() : List<UserAggregateState> {
+        throw IllegalAccessException()
+    }
+
+    @PostMapping("/")
+    fun createUser(@RequestBody nickname: String, @RequestBody name: String, @RequestBody password: String) : UserRegisteredEvent {
+        return userEsService.create { it.registerUser(UUID.randomUUID(), nickname, name, password) }
+    }
+    // TODO DTO
+}
+
--- src/main/kotlin/ru/quipy/logic/aggregateCommands/UserAggregateCommands.kt
+++ src/main/kotlin/ru/quipy/logic/aggregateCommands/UserAggregateCommands.kt
@@ -1,21 +1,12 @@
 package ru.quipy.logic.aggregateCommands
 
-import ru.quipy.api.*
 import ru.quipy.api.aggregatesEvents.UserRegisteredEvent
-import ru.quipy.logic.ProjectAggregateState
 import ru.quipy.logic.aggregateStates.UserAggregateState
 import java.util.*
 
 // Commands : takes something -> returns event
 // Here the commands are represented by extension functions, but also can be the class member functions
 
-fun ProjectAggregateState.create(id: UUID, title: String, creatorId: String): ProjectCreatedEvent {
-    return ProjectCreatedEvent(
-            projectId = id,
-            title = title,
-            creatorId = creatorId,
-    )
-}
 
 fun UserAggregateState.registerUser(userId: UUID, name: String, nickname: String, password: String): UserRegisteredEvent {
     // TODO Uniq check
